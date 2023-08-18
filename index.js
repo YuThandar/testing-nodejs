156662e322df939d9e3cd959da55282e57219f34
Yu Thandar <yuthandar@yig.local>
2023-08-18 16:00:17 +0630

    Initial Nodejs Project

---
+++ index.js
@@ -0,0 +1,70 @@
+const fs = require('fs');
+const http = require('http');
+const url = require('url');
+
+// Replace Function
+const replaceTemplate = (temp, product) => {
+    let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+    output = output.replace(/{%IMAGE%}/g, product.image);
+    output = output.replace(/{%PRICE%}/g, product.price);
+    output = output.replace(/{%FROM%}/g, product.from);
+    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+    output = output.replace(/{%QUANTITY%}/g, product.quantity);
+    output = output.replace(/{%DESCRIPTION%}/g, product.description);
+    output = output.replace(/{%ID%}/g, product.id);
+
+    if(!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not_organic');
+
+    return output;
+}
+
+// Read Template (only one time calling the template file so using synchronous )
+const tempOverview = fs.readFileSync(`${__dirname}/templates/template_overview.html`, 'utf-8');
+const tempProduct = fs.readFileSync(`${__dirname}/templates/template_product.html`, 'utf-8');
+const tempCard = fs.readFileSync(`${__dirname}/templates/template_card.html`, 'utf-8');
+
+// Read Data From JSON
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+const dataObj = JSON.parse(data);
+// console.log(dataObj);
+
+// Create Server
+const server = http.createServer((req, res) => {
+    // console.log(req.url);
+    // console.log(url.parse(req.url, true)); // true parameter is( url's query convert object eg /product?id=2 => {id: '2'})
+
+    const {query, pathname: pathName } = url.parse(req.url, true);
+    // console.log(query);
+    console.log(url.parse(req.url, true));
+
+    // const pathName = req.url;
+    
+    // Home Page
+    if(pathName === '/' || pathName === '/overview') {
+        res.writeHead(200, {
+            'Content-type': 'text/html'
+        })
+        // const cardHTML = dataObj.map((el => replaceTemplate(tempCard, el))); // array html template (so we need to convert array to string)
+        // console.log(cardHTML); 
+
+        const cardHTML = dataObj.map((el => replaceTemplate(tempCard, el))).join(''); // so we need to convert array to string using 'join' method
+
+        const output = tempOverview.replace(/{%PRODUCT_CARDS%}/g, cardHTML);
+        // console.log(cardHTML); 
+
+        res.end(output);
+
+    // Overview Page
+    }
+    else if(pathName === '/product') {
+        res.end('Product Page');
+     // Error Page
+    } else {
+        res.end('404 Not Found!')
+    }
+})
+
+// Listen Request
+server.listen(8000, '127.0.0.1', () => {
+    console.log('Listening To Request on port 8000');
+})
